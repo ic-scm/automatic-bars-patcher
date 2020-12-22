0769b6dc4fcb213950eae0af3e3775afc6d253cc
IC <ic@smashcustommusic.net>
2020-12-22 21:32:39 +0100

    Switch: Path editor

--- switch/src/main.cpp
+++ switch/src/main.cpp
@@ -68,6 +68,7 @@ int main(int argc, char** args) {
         uint16_t selected_game = 0;
         bool text_refresh = 1;
         bool confirmed = 0;
+        bool editing_paths = 0;
         
         while(1) {
             if(!appletMainLoop()) goto main_exit;
@@ -117,50 +118,136 @@ int main(int argc, char** args) {
         
         //Stage 2: Confirm and edit game settings.
         
-        //Resolve for * in all paths
-        path_resolve(config_storage->entries[selected_game]->bars_path);
-        path_resolve(config_storage->entries[selected_game]->stream_dir);
-        path_resolve(config_storage->entries[selected_game]->mod_stream_dir);
-        path_resolve(config_storage->entries[selected_game]->output_bars_path);
-        
-        printf("\x1b[35mOriginal BARS file: \x1b[0m%s\n\n\x1b[35mOriginal BWAV folder: \x1b[0m%s\n\n\x1b[35mModded BWAV folder: \x1b[0m%s\n\n\x1b[35mPatched BARS output: \x1b[0m%s\n\n", 
-            config_storage->entries[selected_game]->bars_path,
-            config_storage->entries[selected_game]->stream_dir,
-            config_storage->entries[selected_game]->mod_stream_dir,
-            config_storage->entries[selected_game]->output_bars_path
-        );
-        
-        printf("\x1b[36mAre these settings correct?\x1b[0m \x1b[33m( (A) Accept & Patch   (B) Edit )\x1b[0m\n");
-        
-        while(1) {
-            if(!appletMainLoop()) goto main_exit;
-            //Refresh inputs
-            padUpdate(&pad);
-            kDown = padGetButtonsDown(&pad);
-            //Exit on (+) press
-            if(kDown & HidNpadButton_Plus) goto main_exit;
-            //Confirm and break on (A) press
-            if(kDown & HidNpadButton_A) {
-                confirmed = 1;
-                text_refresh = 1;
-            }
+        //Running in a loop when editing paths.
+        editing_paths = 1;
+        while(editing_paths) {
+            editing_paths = 0;
             
-            //TODO: editing config
+            //Resolve for * in all paths
+            path_resolve(config_storage->entries[selected_game]->bars_path);
+            path_resolve(config_storage->entries[selected_game]->stream_dir);
+            path_resolve(config_storage->entries[selected_game]->mod_stream_dir);
+            path_resolve(config_storage->entries[selected_game]->output_bars_path);
             
-            //Selection display
-            if(text_refresh) {
-                text_refresh = 0;
-                //Break on confirmed flag
-                if(confirmed) {
-                    printf("\n");
-                    confirmed = 0;
-                    break;
+            printf("\x1b[35mOriginal BARS file: \x1b[0m%s\n\n\x1b[35mOriginal BWAV folder: \x1b[0m%s\n\n\x1b[35mModded BWAV folder: \x1b[0m%s\n\n\x1b[35mPatched BARS output: \x1b[0m%s\n\n", 
+                   config_storage->entries[selected_game]->bars_path,
+                   config_storage->entries[selected_game]->stream_dir,
+                   config_storage->entries[selected_game]->mod_stream_dir,
+                   config_storage->entries[selected_game]->output_bars_path
+            );
+            
+            printf("\x1b[36mAre these settings correct?\x1b[0m \x1b[33m( (A) Accept & Patch   (B) Edit )\x1b[0m\n");
+            
+            while(1) {
+                if(!appletMainLoop()) goto main_exit;
+                //Refresh inputs
+                padUpdate(&pad);
+                kDown = padGetButtonsDown(&pad);
+                //Exit on (+) press
+                if(kDown & HidNpadButton_Plus) goto main_exit;
+                //Confirm and break on (A) press
+                if(kDown & HidNpadButton_A) {
+                    confirmed = 1;
+                    text_refresh = 1;
                 }
+                //Break and go into edit mode on (B) press
+                if(kDown & HidNpadButton_B) {
+                    confirmed = 1;
+                    text_refresh = 1;
+                    editing_paths = 1;
+                }
+                
+                //Selection display
+                if(text_refresh) {
+                    text_refresh = 0;
+                    //Break on confirmed flag
+                    if(confirmed) {
+                        printf("\n");
+                        confirmed = 0;
+                        break;
+                    }
+                }
+                
+                consoleUpdate(NULL);
             }
             
-            consoleUpdate(NULL);
+            
+            //Stage 2.5 optional: Editing paths
+            if(editing_paths) {
+                
+                //Currently select path. (1-4: bars_path -> output_bars_path, 0: Done)
+                uint32_t selected_path = 0;
+                
+                while(1) {
+                    printf("\x1b[36mWhich setting would you like to edit?\x1b[0m\n\x1b[33m( ( <  ) Original BARS file   (  > ) Original BWAV folder )\x1b[0m\n\x1b[33m( ( /\\ ) Modded BWAV folder   ( \\/ ) Patched BARS output  )\x1b[0m\n");
+                    printf("\x1b[33m( (B) Done )\x1b[0m\n\n");
+                    
+                    //Wait for input
+                    while(1) {
+                        if(!appletMainLoop()) goto main_exit;
+                        //Refresh inputs
+                        padUpdate(&pad);
+                        kDown = padGetButtonsDown(&pad);
+                        //Exit on (+) press
+                        if(kDown & HidNpadButton_Plus) goto main_exit;
+                        
+                        //Take inputs
+                        if(kDown & HidNpadButton_B) { selected_path = 0; break; }
+                        if(kDown & HidNpadButton_Left) { selected_path = 1; break; }
+                        if(kDown & HidNpadButton_Right) { selected_path = 2; break; }
+                        if(kDown & HidNpadButton_Up) { selected_path = 3; break; }
+                        if(kDown & HidNpadButton_Down) { selected_path = 4; break; }
+                        
+                        consoleUpdate(NULL);
+                    }
+                    
+                    if(selected_path == 0) break;
+                    
+                    //Edit path with system keyboard
+                    if(selected_path == 1) {
+                        swkbd_edit_text(config_storage->entries[selected_game]->bars_path, "Original BARS file path");
+                        printf("\x1b[35mNew original BARS file: \x1b[0m%s\n\n", config_storage->entries[selected_game]->bars_path);
+                    }
+                    else if(selected_path == 2) {
+                        swkbd_edit_text(config_storage->entries[selected_game]->stream_dir, "Original BWAV folder path");
+                        printf("\x1b[35mNew original BWAV folder: \x1b[0m%s\n\n", config_storage->entries[selected_game]->stream_dir);
+                    }
+                    else if(selected_path == 3) {
+                        swkbd_edit_text(config_storage->entries[selected_game]->mod_stream_dir, "Modded BWAV folder path");
+                        printf("\x1b[35mNew modded BWAV folder: \x1b[0m%s\n\n", config_storage->entries[selected_game]->mod_stream_dir);
+                    }
+                    else if(selected_path == 4) {
+                        swkbd_edit_text(config_storage->entries[selected_game]->output_bars_path, "Patched BARS output path");
+                        printf("\x1b[35mNew patched BARS output: \x1b[0m%s\n\n", config_storage->entries[selected_game]->output_bars_path);
+                    }
+                }
+                
+                //Ask about saving to config file
+                printf("\x1b[36mWould you like to save the new settings to the config file?\x1b[0m\n\x1b[33m( (A) Yes   (B) No )\x1b[0m\n");
+                //Wait for input, and write config file on A press
+                while(1) {
+                    if(!appletMainLoop()) goto main_exit;
+                    //Refresh inputs
+                    padUpdate(&pad);
+                    kDown = padGetButtonsDown(&pad);
+                    //Exit on (+) press
+                    if(kDown & HidNpadButton_Plus) goto main_exit;
+                    
+                    //Take inputs
+                    if(kDown & HidNpadButton_A) {
+                        //TODO config writer
+                        
+                        break;
+                    }
+                    if(kDown & HidNpadButton_B) break;
+                    
+                    consoleUpdate(NULL);
+                }
+                
+                printf("\n");
+            }
+            
         }
-        
         
         //Stage 3: Main BARS patching.
         //TODO: Run barspatcher_run on another thread
